ca3d032c5150a09134b5c6708a6f1ba02b9c27a6
FabryFdez <fabryfdez@gmail.com>
2016-07-11 19:54:20 -0400

    Changed Root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get '/index' => "foursquare#index"
 get '/search' => 'foursquare#search'
+root 'foursquare#search'
 end
